8c244136d7ccd6cba66b531492cd398ec35360e6
Adriana Venegas <adrivegas08@gmail.com>
2021-06-01 09:22:47 -0500

    Listview-mostrar una lista de elementos

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Componentes App',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Componentes'),
-          ),
-          body: HomePageTemp()),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Componentes App',
+        home: HomePageTemp());
   }
 }
