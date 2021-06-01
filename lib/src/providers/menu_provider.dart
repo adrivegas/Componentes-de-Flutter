fcc8c80e46ab808105ced9f06adacdf3e3089f05
Adriana Venegas <adrivegas08@gmail.com>
2021-06-01 10:41:15 -0500

    Leer un archivo JSON local

---
+++ lib/src/providers/menu_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/services.dart' show rootBundle;
+
+class _MenuProvider {
+  List<dynamic> opciones = [];
+
+  _MenuProvider() {
+    cargarData();
+  }
+
+  void cargarData() {
+    rootBundle.loadString('data/menu_opts.json').then((data) {
+      print(data);
+    });
+  }
+}
+
+// Este archivo sólo expone la instancia creada de _MenuProvider()
+final menuProvider = new _MenuProvider();
