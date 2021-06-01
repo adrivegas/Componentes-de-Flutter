6c9c19bb0aaee168a850c7ebaad5ce37c6ceceea
Adriana Venegas <adrivegas08@gmail.com>
2021-06-01 12:09:02 -0500

    Utilidades - obtener ícono por strings

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -32,10 +33,7 @@ class HomePage extends StatelessWidget {
     data!.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
-        leading: Icon(
-          Icons.ac_unit,
-          color: Colors.blue.shade200,
-        ),
+        leading: getIcon(opt['icon']),
         trailing: Icon(
           Icons.keyboard_arrow_right,
           color: Colors.blue.shade200,
