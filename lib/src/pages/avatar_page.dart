ff46c4c392454d2a4e3931add2a82c69868fb405
Adriana Venegas <adrivegas08@gmail.com>
2021-06-02 17:32:05 -0500

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.pinimg.com/236x/38/66/e1/3866e143ea4fb842f6b55340d7ca055f--no-se-free-time.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('AV'),
+              backgroundColor: Colors.purple.shade200,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/9/91/Oahu_Landscape.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
