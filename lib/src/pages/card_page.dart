c4fea1bd14c5029489724265cc6a38c08dfed2b4
Adriana Venegas <adrivegas08@gmail.com>
2021-06-02 14:05:16 -0500

    FadeIn Image

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,7 +9,13 @@ class CardPage extends StatelessWidget {
         ),
         body: ListView(
           padding: EdgeInsets.all(10.0),
-          children: [_cardTipo1()],
+          children: [
+            _cardTipo1(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _cardTipo2(),
+          ],
         ));
   }
 
@@ -28,6 +34,27 @@ class CardPage extends StatelessWidget {
               TextButton(onPressed: () {}, child: Text('Cancelar')),
               TextButton(onPressed: () {}, child: Text('OK'))
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: [
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/9/91/Oahu_Landscape.jpg'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Texto de card'),
           )
         ],
       ),
